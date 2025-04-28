a8d4387ecb200db9f2170e4878f8510dd4d9941c
vinicius <vini.Stormtrooper@gmail.com>
2025-04-28 11:23:57 -0300

    alterando o exercicio 4

--- encapsulamento/exercicios/exercicio4/Produto.dart
+++ encapsulamento/exercicios/exercicio4/Produto.dart
@@ -2,7 +2,7 @@ class Produto {
   String _nome = '';
   set nome(String palavra){
     String primeiraLetra = palavra.substring(0,1);
-    _nome = primeiraLetra.toUpperCase()+palavra.substring(1);
+    _nome = primeiraLetra.toUpperCase()+palavra.substring(1).toLowerCase();
   }
   String get nome => _nome.trim();
 }
